6e7c5cc36f78af2c6fd6e8063d4c7a2ac6b1cfe8
Daniel Carvalho <mail@danielcarvalho.pt>
2015-01-27 17:30:07 +0000

    Script serial

--- script_serial.sh
+++ script_serial.sh
@@ -1,15 +1,15 @@
-##!/bin/bash 
-#PBS -l walltime=03:00 
-#PBS -j oe 
-#PBS -N a 
-#PBS -l nodes=4:ppn=2 
+#!/bin/bash
+qsub -I -l nodes=4:ppn=2 -lwalltime=15:00
+#PBS -j oe
+#PBS -N a
+#PBS -l nodes=4:ppn=2
 #PBS -q mei
- 
-cat $PBS_NODEFILE 
-cd ~/PCP 
 
-#valgrind --toold=callgrind --cache-sim=yes --separate-threads=yes omp_mat_vect_rand_split
+cat $PBS_NODEFILE
+~/Hybrid
+#valgrind --toold=callgrind --cache-sim=yes --separate-threads=yes serial
 module add gnu/4.9.0
+g++ -O3 jacobi_serial.c -o serial
 
 export OMPP_CTR1 = PAPI_L2_DCW
 export OMPP_CTR4 = PAPI_L3_DCW
@@ -21,14 +21,14 @@ do
 echo export OMP_NUM_THREADS = $th
 export OMP_NUM_THREADS=$th
 
-export OMPP_APPNAME=omp_mat_vect_rand_split-8000000x8
-echo ./kinst-omp_mat_vect $th 8000000 8
-./kinst-omp_mat_vect $th 8000000 8
+export OMPP_APPNAME=serial-8000000x8
+echo ./serial $th 8000000 8
+./serial $th 8000000 8
 
-export OMPP_APPNAME=omp_mat_vect_rand_split-8000x8000
-echo ./kinst-omp_mat_vect $th 8000 8000
-./kinst-omp_mat_vect $th 8000 8000
+export OMPP_APPNAME=serial-8000x8000
+echo ./serial $th 8000 8000
+./serial $th 8000 8000
 
-export OMPP_APPNAME=omp_mat_vect_rand_split-8x8000000
-echo ./kinst-omp_mat_vect $th 8 8000000
-./kinst-omp_mat_vect $th 8 8000000
+export OMPP_APPNAME=serial-8x8000000
+echo ./serial $th 8 8000000
+./serial $th 8 8000000
